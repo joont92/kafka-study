575a3f46df5c077f7e9a5cf48d5f2a0f13bc0633
joont92 <pk920207@gmail.com>
2020-05-06 23:27:47 +0900

    init

---
+++ kafka-프로듀서.md
@@ -0,0 +1,4 @@
+# 프로듀서 주요 옵션
+- bootstrap.servers
+- acks
+  - acks=0
---
+++ producer/step01/produce.sh
@@ -0,0 +1,1 @@
+~/workspace/kafka-study/kafka_2.5.0/bin/kafka-console-producer.sh --broker-list localhost:19092,localhost:29092,localhost:39092 --topic peter-topic1
